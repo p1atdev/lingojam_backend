23f727a0e9611b4e94c0b5836752888ddffcbd68
p1atdev <shu_teiei@outlook.jp>
2022-04-24 00:34:47 +0900

    fix: date parsing

--- src/model/parser.ts
+++ src/model/parser.ts
@@ -1,3 +1,4 @@
+import { parse } from "date-fns"
 import { JSDOM } from "jsdom"
 import { Episode } from "../types/episode"
 import { Sentence, Transcript } from "../types/transcript"
@@ -38,138 +39,6 @@ export class Parser {
             return null
         }
         const elements = Array.from(document.querySelectorAll("*"))
-
-        // // The story…
-        // const titleEl = getSentence(elements, "The story…")
-        // if (!titleEl) {
-        //     console.log("No title")
-        //     return
-        // }
-        // if (!titleEl.textContent) {
-        //     console.log("No title text")
-        //     return
-        // }
-        // const title = await this.translator.enToJaText(titleEl.textContent)
-        // console.log(title)
-
-        // // Learn language related to…
-        // const categoryEl = getSentence(elements, "Learn language related to…")
-        // if (!categoryEl) {
-        //     console.log("No category")
-        //     return
-        // }
-        // if (!categoryEl.textContent) {
-        //     console.log("No category text")
-        //     return
-        // }
-        // const category = await this.translator.enToJaText(categoryEl.textContent)
-        // console.log(category)
-
-        // // Need-to-know language… ~ Answer this…
-        // const wordEls = getSentences(elements, "Need-to-know language…", "Answer this…")
-        // if (!wordEls) {
-        //     console.log("No words")
-        //     return
-        // }
-
-        // const wordPairs = await Promise.all(
-        //     wordEls
-        //         .filter((e) => e.tagName == "P")
-        //         .map(async (wordText) => {
-        //             const text = wordText.textContent
-        //             if (!text) {
-        //                 return
-        //             }
-
-        //             const [word, meaning] = text.split("–").map((s) => s.trim())
-
-        //             return {
-        //                 word: await this.translator.enToJaText(word),
-        //                 meaning: await this.translator.enToJaText(meaning),
-        //             }
-        //         })
-        // )
-
-        // console.log(wordPairs)
-
-        // // Answer this…
-        // const questionEl = getSentence(elements, "Answer this…")
-        // if (!questionEl) {
-        //     console.log("No question")
-        //     return
-        // }
-        // if (!questionEl.textContent) {
-        //     console.log("No question text")
-        //     return
-        // }
-        // const question = await this.translator.enToJaText(questionEl.textContent)
-        // console.log(question)
-
-        // // Transcript ~ Did you get it?
-        // const transcriptEl = getSentences(elements, "Transcript", "Did you get it?")
-        // if (!transcriptEl) {
-        //     console.log("No transcript")
-        //     return
-        // }
-
-        // const sentences = (
-        //     await Promise.all(
-        //         transcriptEl
-        //             .filter((e) => e.tagName == "P")
-        //             .map(async (text) => {
-        //                 // console.log("sentence:", sentence)
-        //                 // console.log("sentence.tagName:", sentence.tagName)
-        //                 // console.log("sentence.textContent:", sentence.textContent)
-
-        //                 const content = text.textContent
-        //                 if (!content) {
-        //                     return
-        //                 }
-
-        //                 const sentence: Sentence = {
-        //                     text: await this.translator.enToJaText(content),
-        //                     type: text.innerHTML.startsWith("<strong>") ? "heading" : "paragraph",
-        //                 }
-
-        //                 return sentence
-        //             })
-        //     )
-        // ).filter((s): s is Sentence => {
-        //     return s != undefined && s != null
-        // })
-
-        // const transcript: Transcript = {
-        //     sentences: sentences,
-        // }
-
-        // console.dir(transcript, { depth: null })
-
-        // const answerEls = (() => {
-        //     const answerIndex = elements.findIndex((e) => e.textContent == "Did you get it?")
-        //     if (answerIndex == -1) {
-        //         return undefined
-        //     }
-
-        //     return elements.slice(answerIndex + 2)
-        // })()
-        // if (!answerEls) {
-        //     console.log("No answer")
-        //     return
-        // }
-        // const answer: Translation[] = (
-        //     await Promise.all(
-        //         answerEls.map(async (e) => {
-        //             if (!e.textContent) {
-        //                 return
-        //             }
-        //             return await this.translator.enToJaText(e.textContent)
-        //         })
-        //     )
-        // ).filter((s): s is Translation => {
-        //     return s != undefined && s != null
-        // })
-
-        // console.log(answer)
 
         try {
             const episodeNumber = this.parseEpisodeNumber()
@@ -226,12 +95,19 @@ export class Parser {
         if (!dateEl) {
             throw new Error("No date")
         }
-        const date = dateEl.innerHTML.split("/")[1].trim()
-        return new Date(date)
+
+        const dateText = dateEl.textContent?.split("/")[1].trim() ?? ""
+
+        const date = parse(dateText, "d MMM yyyy", new Date())
+        return date
     }
 
     parseVideoURL = (): string | null => {
+        const iframe = this.document.querySelector("iframe")?.innerHTML
+        console.log(iframe)
+        console.log(iframe?.includes("568ec290-626e-4ddb-83fa-6b52928eb728"))
         return ""
+
         // const document = this.document.querySelector("iframe#smphtml5iframebbcMediaPlayer0")
         // if (!document) {
         //     throw new Error("No content")
